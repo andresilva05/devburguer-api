56411242fa63bb8aa63f4b5c99fa28331fc051b1
andresilva05 <andresilva9657@gmail.com>
2024-11-11 17:28:34 -0300

    Refatora inicialização e sincronização de modelos com o banco de dados

--- src/database/index.js
+++ src/database/index.js
@@ -1,34 +1,41 @@
+import mongoose from 'mongoose'; // Certifique-se de importar o mongoose
 import Sequelize from 'sequelize';
+import Category from '../app/models/Category.js';
 import Product from '../app/models/Product.js';
 import User from '../app/models/user.js';
-import configDatabase from '../config/database.cjs'; // Certifique-se de que a extensão está correta
+import configDatabase from '../config/database.cjs'; // Verifique a extensão do arquivo de configuração
 
-const models = [User, Product];
+const models = [User, Product, Category];
 
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
     // Inicializa a conexão com o banco de dados usando a configuração
     this.connection = new Sequelize(configDatabase);
 
-    // Inicializa todos os modelos
-    models.map((model) => model.init(this.connection));
+    // Inicializa todos os modelos e associações
+    models
+      .map((model) => model.init(this.connection))
+      .map((model) => model.associate && model.associate(this.connection.models));
+  }
 
-    // Sincroniza os modelos com o banco de dados
-    this.syncModels();
+  mongo() {
+    this.mongoConnection = mongoose.connect('mongodb://localhost:27017/devburguer',
+    )
   }
 
   // Método para sincronizar os modelos com o banco de dados
   syncModels() {
-    this.connection.sync({ alter: true }) // ou { force: true } para recriar tabelas
+    this.connection
+      .sync({ alter: true }) // ou { force: true } para recriar tabelas
       .then(() => {
         console.log('Modelos sincronizados com o banco de dados.');
       })
       .catch((error) => {
-        
         console.error('Erro ao sincronizar modelos com o banco de dados:', error);
       });
   }
