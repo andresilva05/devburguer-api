dbdde2fb0bdb6b706e034664c6ae1378ac02f2c4
andresilva05 <andresilva9657@gmail.com>
2024-11-12 20:23:48 -0300

    feat: criação da tabela 'users' no banco de dados
    - Criada a tabela 'users' no banco de dados utilizando Sequelize com os seguintes campos:
      - `id`: UUID como chave primária, gerado automaticamente com `UUIDV4`.
      - `name`: Nome do usuário (obrigatório).
      - `email`: Email do usuário (obrigatório e único).
      - `password_hash`: Armazenamento da senha criptografada (obrigatório).
      - `admin`: Campo booleano para indicar se o usuário é administrador (valor padrão: `false`).
      - `created_at`: Data de criação do usuário (com valor padrão como o timestamp atual).
      - `updated_at`: Data de atualização do usuário (com valor padrão como o timestamp atual).
    
    - A migração inclui a criação da tabela 'users' com a estrutura acima e o método `down` para deletá-la, caso necessário.
    
    Nota: As colunas `created_at` e `updated_at` são definidas para serem geradas automaticamente, sem o uso do `ON UPDATE` no banco de dados.

--- src/database/migrations/20241014204521-create-users-table.cjs
+++ src/database/migrations/20241014204521-create-users-table.cjs
@@ -30,12 +30,11 @@ module.exports = {
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'), // Define o valor padrão como o timestamp atual
+    
       },
       updated_at: {
         type: Sequelize.DATE,
         allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'), // Remove ON UPDATE, padrão apenas
       }
     });
   },
