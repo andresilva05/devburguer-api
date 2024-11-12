6b06bdacfd14e076fdce094c8a20b9e5d2749621
andresilva05 <andresilva9657@gmail.com>
2024-11-12 20:23:14 -0300

    feat: configurar banco de dados com Sequelize e MongoDB
    - Configurada a conexão com o banco de dados SQL usando Sequelize, com base na configuração fornecida em `configDatabase`.
    - Inicializados os modelos `User`, `Product` e `Category`, realizando a associação entre eles onde necessário.
    - Adicionada a conexão com MongoDB utilizando Mongoose, com o banco de dados `devburguer` como alvo.
    - O método `syncModels()` foi adicionado para sincronizar os modelos com o banco de dados, permitindo a atualização automática das tabelas.

--- src/database/index.js
+++ src/database/index.js
@@ -1,43 +1,32 @@
-import mongoose from 'mongoose';
-import Sequelize from 'sequelize';
-import configDatabase from '../config/database.cjs';
-import User from '../app/models/User.js';
-import Product from '../app/models/Product.js';
-import Category from '../app/models/Category.js';
+import Sequelize from 'sequelize'
+import mongoose from 'mongoose'
+import configDatabase from '../config/dataBase'
 
-const models = [User, Product, Category];
+
+import User from '../app/models/User'
+import Product from '../app/models/Product'
+import Category from '../app/models/Category'
+
+const models = [User, Product, Category]
 
 class Database {
   constructor() {
-    this.init();
-    this.mongo();
+    this.init()
+    this.mongo()
   }
 
   init() {
-    // Inicializa a conexão com o banco de dados usando a configuração
-    this.connection = new Sequelize(configDatabase);
-
-    // Inicializa todos os modelos e associações
+    this.connection = new Sequelize(configDatabase)
     models
       .map((model) => model.init(this.connection))
-      .map((model) => model.associate && model.associate(this.connection.models));
+      .map(
+        (model) => model.associate && model.associate(this.connection.models),
+      )
   }
 
-  mongo() {
-    this.mongoConnection = mongoose.connect('mongodb://localhost:27017/devburguer');
-  }
-  
-  // Método para sincronizar os modelos com o banco de dados
-  syncModels() {
-    this.connection
-      .sync({ alter: true }) // ou { force: true } para recriar tabelas
-      .then(() => {
-        console.log('Modelos sincronizados com o banco de dados.');
-      })
-      .catch((error) => {
-        console.error('Erro ao sincronizar modelos com o banco de dados:', error);
-      });
+  mongo(){
+    this.mongoConnection = mongoose.connect('mongodb://localhost:27017/devburger')
   }
 }
 
-export default new Database();
+export default new Database()
