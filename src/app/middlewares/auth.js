bc2be4393de3e9a63504b8994fc5ec9ec9c13322
andresilva05 <andresilva9657@gmail.com>
2024-11-12 20:22:01 -0300

    feat: adicionar middleware de autenticação JWT
    - Implementado middleware para verificar a autenticidade do token JWT.
    - O token é extraído do cabeçalho de autorização da requisição.
    - Se o token não for fornecido ou for inválido, é retornado um erro de 401 (não autorizado).
    - Se o token for válido, as informações do usuário (ID e nome) são extraídas e adicionadas ao objeto `request`, permitindo o acesso a essas informações nas próximas etapas do processamento.
    - A função `jwt.verify()` é usada para validar o token com a chave secreta definida em `authConfig`.
    - O middleware chama `next()` após a verificação bem-sucedida para passar o controle para o próximo middleware ou rota.

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,28 +1,38 @@
-import jwt from 'jsonwebtoken';
-import authConfig from '../../config/auth.js';
 
-function authMiddleware(request, response, next) {
-  const authToken = request.headers.authorization;
+import jwt from 'jsonwebtoken'
+import authConfig from '../../config/auth'
 
-  if (!authToken) {
-    return response.status(401).json({ error: 'Token not provided' });
-  }
+export default function authMiddleware(request, response, next) {
 
-  const token = authToken.split(' ')[1];
+    // console.log(request.headers.authorization);
 
-  jwt.verify(token, authConfig.secret, (err, decoded) => {
-    if (err) {
-      return response.status(401).json({ error: 'Invalid token' });
+    const authToken = request.headers.authorization;
+
+    if (!authToken) {
+
+        return response.status(401).json({ error: 'Token not provid' })
     }
-    
 
-    // Define as informações do usuário no objeto `request` para uso posterior
-    request.userId = decoded.id;
-    request.userName = decoded.name;
+    const token = authToken.split(' ').at(1)
+    // const [_,token] = authToken.split(' ')[1]
 
-    // Chama o próximo middleware ou rota após a verificação do token
+    try {
+
+        jwt.verify(token, authConfig.secret, (err, decoded)=>{
+
+            if(err){
+                throw new Error();
+            }
+            
+            request.userId = decoded.id
+            request.userName = decoded.name
+            
+        })
+
+    } catch (err) {
+        return response.status(401).json({error: 'Token is invalid'})
+    }
+
     return next();
-  });
 }
 
-export default authMiddleware;
