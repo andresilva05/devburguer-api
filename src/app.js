831f8760a884bcaa2886148e9539f2f276bdeaba
andresilva05 <andresilva9657@gmail.com>
2024-11-28 22:32:05 -0300

    feat: adiciona suporte ao CORS e configura middlewares de upload de arquivos

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express'; // Importa o Express
 import { resolve } from 'node:path';
 import './database'; // Importa e inicializa a conexão com o banco de dados
 import routes from './routes.js'; // Importa suas rotas
-
+import cors from 'cors'; // Importa o CORS
 
 class App {
   constructor() {
@@ -13,15 +13,15 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors()); // Adiciona o middleware CORS
     this.app.use(express.json()); // Configura o middleware para JSON
-    this.app.use('/product-file',express.static(resolve(__dirname,'..','uploads')))
-    this.app.use('/category-file',express.static(resolve(__dirname,'..','uploads')))
+    this.app.use('/product-file', express.static(resolve(__dirname, '..', 'uploads')));
+    this.app.use('/category-file', express.static(resolve(__dirname, '..', 'uploads')));
   }
 
   routes() {
     this.app.use(routes); // Usa as rotas importadas
   }
-
 }
 
 export default new App().app; // Exporta uma instância da classe App
