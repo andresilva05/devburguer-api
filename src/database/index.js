8773a11b4854305e49021df7e2f888b429ce6c66
andresilva05 <andresilva9657@gmail.com>
2024-12-20 20:02:03 -0300

    Refactor: Conexão com bancos de dados (Sequelize e MongoDB) com variáveis de ambiente

--- src/database/index.js
+++ src/database/index.js
@@ -1,32 +1,41 @@
-import Sequelize from 'sequelize'
-import mongoose from 'mongoose'
-import configDatabase from '../config/dataBase'
+import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+import configDatabase from '../config/dataBase';
 
+require('dotenv').config(); // Carregar variáveis de ambiente
 
-import User from '../app/models/User'
-import Product from '../app/models/Product'
-import Category from '../app/models/Category'
+import User from '../app/models/User';
+import Product from '../app/models/Product';
+import Category from '../app/models/Category';
 
-const models = [User, Product, Category]
+const models = [User, Product, Category];
 
 class Database {
   constructor() {
-    this.init()
-    this.mongo()
+    this.init();
+    this.mongo();
   }
 
   init() {
-    this.connection = new Sequelize(configDatabase)
+    this.connection = new Sequelize(configDatabase);
     models
       .map((model) => model.init(this.connection))
       .map(
         (model) => model.associate && model.associate(this.connection.models),
-      )
+      );
   }
 
-  mongo(){
-    this.mongoConnection = mongoose.connect('mongodb://localhost:27017/devburger')
+  mongo() {
+    const mongoUri =
+      process.env.MONGO_URI || 'mongodb://localhost:27017/devburger';
+    this.mongoConnection = mongoose
+      .connect(mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log('MongoDB conectado com sucesso!'))
+      .catch((error) => console.error('Erro ao conectar ao MongoDB:', error));
   }
 }
 
-export default new Database()
+export default new Database();
