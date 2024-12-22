e6e02a1c840d0724b5ba47b2451de9cd441d34cc
andresilva05 <andresilva9657@gmail.com>
2024-12-22 13:03:53 -0300

    chore: remover opções de configuração desatualizadas da conexão do MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -29,10 +29,7 @@ class Database {
     const mongoUri =
       process.env.MONGO_URI || 'mongodb://localhost:27017/devburger';
     this.mongoConnection = mongoose
-      .connect(mongoUri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
+      .connect(mongoUri)
       .then(() => console.log('MongoDB conectado com sucesso!'))
       .catch((error) => console.error('Erro ao conectar ao MongoDB:', error));
   }
